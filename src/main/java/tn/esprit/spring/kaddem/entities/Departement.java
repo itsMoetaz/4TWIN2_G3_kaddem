b80e05fc2dc333282826b84b223ef7026f054bc8
eyansibi <eya.nsibi@esprit.tn>
2025-04-15 09:57:10 +0100

    sonar test

--- src/main/java/tn/esprit/spring/kaddem/entities/Departement.java
+++ src/main/java/tn/esprit/spring/kaddem/entities/Departement.java
@@ -1,10 +1,5 @@
 package tn.esprit.spring.kaddem.entities;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
 import java.io.Serializable;
-import java.util.Set;
-
 import javax.persistence.*;
 
 @Entity
